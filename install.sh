fd7cecb87f9b2862dbee5011eacebd35a4af01f4
Cassidy James Blaede <c@ssidyjam.es>
2020-07-04 22:18:56 -0600

    Update to new repo name

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ln -s ~/Projects/cassidyjames/dotfiles/.bash_aliases ~/.bash_aliases
-ln -s ~/Projects/cassidyjames/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/Projects/cassidyjames/dotfiles/.hidden ~/.hidden
-ln -s ~/Projects/cassidyjames/dotfiles/.inputrc ~/.inputrc
+ln -s ~/Projects/cassidyjames/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/Projects/cassidyjames/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Projects/cassidyjames/.dotfiles/.hidden ~/.hidden
+ln -s ~/Projects/cassidyjames/.dotfiles/.inputrc ~/.inputrc
 echo "\nAll done. Now restart Terminal or run \`source ~/.bash_aliases\`.\n"
